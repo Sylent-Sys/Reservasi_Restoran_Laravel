ca1776136fe7a3a692ddd8af02a3957a7d632649
jepehh <gabrieljasonpatrick51@gmail.com>
2024-01-05 19:05:01 +0700

    feat: make migrations and model haha <3

--- database/migrations/2024_01_05_110646_create_reservations_table.php
+++ database/migrations/2024_01_05_110646_create_reservations_table.php
@@ -11,9 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('reservations', function (Blueprint $table) {
+        Schema::create('reservation', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->int('user_id');
+            $table->timestamp('reservation_date');
+            $table->string('reservation_by');
+            $table->string('seat_id');
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('seat_id')->references('id')->on('seats');
         });
     }
 
@@ -22,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('reservations');
+        Schema::dropIfExists('reservation');
     }
 };
