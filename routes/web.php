2f0da4fc130d5acd15926a01bed55b2de097e1e5
jepehh <gabrieljasonpatrick51@gmail.com>
2024-01-10 04:23:34 +0700

    feat: lagi cari tau cara bikin dashboard ui pake php, ini abis ketemu di themewagon

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DropzoneController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,5 @@ Route::get('/', function () {
 
 
 Route::get('/dashboard', [DashboardController::class, 'index']);
+
+Route::get('/dropzone', [DropzoneController::class, 'showDropzone']);
